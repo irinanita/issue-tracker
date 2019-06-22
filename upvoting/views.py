68296bb625453c81b22bfcc415de66a8fdfdae57
Irina Nita <irinanita.im@gmail.com>
2019-06-22 15:28:53 +0000

    Mover alert related to ticket update to the top

--- upvoting/views.py
+++ upvoting/views.py
@@ -19,12 +19,12 @@ def upvote(request,pk):
             ticket.score+=1
             ticket.save()
             print(ticket.score,'current ticket score')
-            messages.success(request,"You have successfully voted ")
+            messages.success(request,"You have successfully voted ", extra_tags="ticket-update")
         else:
-            messages.success(request,"You need to pay first ")
+            messages.success(request,"You need to pay first ", extra_tags="ticket-update")
             print('ticket score', ticket.score)
     else:
-        messages.error(request,"You have alreaty voted for this")
+        messages.error(request,"You have alreaty voted for this", extra_tags="ticket-update")
     return redirect ("ticket_details",pk=ticket.id)
     
   
