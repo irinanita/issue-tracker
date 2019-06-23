080401b521fa100a11154269cafa9816e1ddae29
Irina Nita <irinanita.im@gmail.com>
2019-06-23 19:44:45 +0000

    Added checkout tables to admin area

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order, OrderLineTicket
 
 # Register your models here.
+
+'''
+TabularInline subclass defines the template used to render the Order in the
+admin interface. StackedInline is another option
+'''
+class OrderLineAdminInline(admin.TabularInline):
+    model=OrderLineTicket
+
+class OrderAdmin(admin.ModelAdmin):
+    '''
+    The admin interface has the ability to edit more than one model on a 
+    single page. This is known as inlines
+    '''
+    inlines=(OrderLineAdminInline, )
+
+admin.site.register(Order,OrderAdmin)    
