31051007f1f77fafda86248fdb5009a334589f5e
Irina Nita <irinanita.im@gmail.com>
2019-06-25 20:14:48 +0000

    Added removing item from cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import view_cart
+from .views import view_cart,delete_product
 
 urlpatterns = [
     url(r'^$',view_cart,name='view_cart'),
+    url(r'^delete/(?P<pk>\d+)',delete_product,name="delete_product"),
    ]
     
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render,get_object_or_404
-from tickets.models import Ticket
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib import messages
 
 # Create your views here.
 def view_cart(request):
   
     return render(request,'cart.html')
+
+def delete_product(request,pk):
+    cart = request.session.get('cart',{})
+    cart.pop(pk)
+    request.session['cart']=cart
+    messages.success(request, "You have successfully removed one item from your cart")
+    return redirect ('view_cart')    
     
