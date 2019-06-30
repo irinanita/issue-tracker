bb31c028d5a38bfa537f686a44adcb75bba2f220
Irina Nita <irinanita.im@gmail.com>
2019-06-30 20:08:08 +0100

    Implemented password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
-from .views import registration,user_logout,user_login
+from django.conf.urls import url, include
+from .views import registration, user_logout, user_login
+from accounts import url_reset
 
 urlpatterns = [
-    url(r'^registration/$',registration,name="registration"),
-    url(r'^logout/$', user_logout, name='user_logout'),
-    url(r'^login/$', user_login, name='user_login')
+    url(r'^registration/$', registration, name = "registration"),
+    url(r'^logout/$', user_logout, name = 'user_logout'),
+    url(r'^login/$', user_login, name = 'user_login'),
+    url(r'^password-reset/', include(url_reset))
 ]
