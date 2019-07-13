72c3515f318ad0a92fd14f9fd11f0a738de4f1ef
Irina Nita <irinanita.im@gmail.com>
2019-07-13 12:48:58 +0100

    Styling and adjusting NavBar layout

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,21 +1,20 @@
 from django.shortcuts import get_object_or_404
 from tickets.models import Ticket
 
+
 def cart_contents(request):
     '''
     Ensures that the cart contents are available to view when rendering every page
     It will request an existing cart or a blank dictionary if there is none
     '''
-    cart = request.session.get('cart',{})
-    
-    cart_items=[]
-    
-    total_value=0
-    
-    for pk, quantity in cart.items():
-        ticket=get_object_or_404(Ticket,pk=pk)
-        total_value+=15
-        cart_items.append({"pk":pk,"ticket":ticket})
-    print('cart items',cart_items)
+    cart = request.session.get('cart', {})
 
-    return {"cart_items":cart_items,"total_value":total_value}
+    cart_items = []
+    total_value = 0
+
+    for pk, quantity in cart.items():
+        ticket = get_object_or_404(Ticket, pk = pk)
+        total_value += 15
+        cart_items.append({"pk": pk, "ticket": ticket})
+
+    return {"cart_items": cart_items, "total_value": total_value}
--- cart/views.py
+++ cart/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+
 
 # Create your views here.
 def view_cart(request):
-  
-    return render(request,'cart.html')
+    return render(request, 'cart.html')
 
-def delete_product(request,pk):
-    cart = request.session.get('cart',{})
+
+def delete_product(request, pk):
+    cart = request.session.get('cart', {})
     cart.pop(pk)
-    request.session['cart']=cart
+    request.session['cart'] = cart
     messages.success(request, "You have successfully removed one item from your cart")
-    return redirect ('view_cart')    
-    
+    return redirect('view_cart')
