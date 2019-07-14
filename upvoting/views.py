89ebee2d38ee9aaf9b44f4cdc15866b166c7fa04
Irina Nita <irinanita.im@gmail.com>
2019-07-14 19:33:50 +0100

    Removed old messages from ticketdetails. Added sweetalert to upvoting & a modal for voting for features

--- upvoting/views.py
+++ upvoting/views.py
@@ -1,32 +1,32 @@
-from django.shortcuts import render,reverse,redirect,get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import UserTicketVote
 from tickets.models import Ticket
-from django.contrib import messages
+import sweetify
+
 
 # Create your views here.
 @login_required
-def upvote(request,pk):
-    ticket = get_object_or_404(Ticket,pk=pk) if pk else None
+def upvote(request, pk):
+    ticket = get_object_or_404(Ticket, pk = pk) if pk else None
     user_ticket_vote = UserTicketVote.objects.all()
-    user_ticket_vote = user_ticket_vote.filter(Q(ticketID=ticket.id) & Q(user=request.user)).count()
-    print('ticket type',ticket.type)
-    if user_ticket_vote==0:
+    user_ticket_vote = user_ticket_vote.filter(Q(ticketID = ticket.id) & Q(user = request.user)).count()
+    if user_ticket_vote == 0:
         if ticket.type == "bug":
-            user_vote=UserTicketVote(ticketID=ticket,user=request.user)
+            user_vote = UserTicketVote(ticketID = ticket, user = request.user)
             user_vote.save()
-            ticket.score+=1
+            ticket.score += 1
             ticket.save()
-            messages.success(request,"You have successfully voted ", extra_tags="ticket-update")
+            sweetify.success(request, 'You have successfully voted')
         else:
-            cart=request.session.get('cart',{})
+            sweetify.info(request, 'In order to vote for a feature a contribution of 15 EUR is required',
+                          persistent = 'Proceed',
+                          footer = '<a href="http://127.0.0.1:8000/tickets/type/3/"> Go back to Ticket List</a>')
+            cart = request.session.get('cart', {})
             cart[pk] = 1
             request.session['cart'] = cart
-            messages.success(request,"In order to upvote a feature a small contribution of 15 EUR per feature is required. Remember you can only vote once")
-            return redirect ("view_cart")
+            return redirect("view_cart")
     else:
-        messages.error(request,"You have alreaty voted for this", extra_tags="ticket-update")
-    return redirect ("ticket_details",pk=ticket.id)
-    
-  
+        sweetify.error(request, 'Sorry', text = 'You have already voted for this feature')
+    return redirect("ticket_details", pk = ticket.id)
