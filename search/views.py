66ee8b015e3b1751e906d15130c3104d112a8f24
Irina Nita <irinanita.im@gmail.com>
2019-07-21 14:00:03 +0100

    Preserve search/filter parameters when applied

--- search/views.py
+++ search/views.py
@@ -8,33 +8,35 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Will return true if both conditions are met
 def is_valid_query_param(param):
-    return param !='' and param is not None
+    return param != '' and param is not None
+
 
 def search(request):
     # fetch all tickets unfiltered
-    query_set=Ticket.objects.all()
+    query_set = Ticket.objects.all()
     # check if the submitted form was a filter/search or a reset
-    if request.GET.get('search') !='' and request.GET.get('search') is not None:
+    if request.GET.get('search') != '' and request.GET.get('search') is not None:
         query_keyword = request.GET.get('q')
-        query_status=request.GET.get('status')
-        query_label=request.GET.get('label')
-        query_sort=request.GET.get('sort')
-     
-        if  is_valid_query_param(query_keyword):
-        #Searching whether keywords are present in the title or in the description
-            query_set = query_set.filter(Q(title__icontains=query_keyword) | Q(description__icontains=query_keyword)).distinct()
+        query_status = request.GET.get('status')
+        query_label = request.GET.get('label')
+        query_sort = request.GET.get('sort')
+
+        if is_valid_query_param(query_keyword):
+            # Searching whether keywords are present in the title or in the description
+            query_set = query_set.filter(
+                Q(title__icontains = query_keyword) | Q(description__icontains = query_keyword)).distinct()
         if is_valid_query_param(query_status):
-            query_set = query_set.filter(status__icontains=query_status)
+            query_set = query_set.filter(status__icontains = query_status)
         if is_valid_query_param(query_label):
-            query_set = query_set.filter(label__icontains=query_label)
+            query_set = query_set.filter(label__icontains = query_label)
         if is_valid_query_param(query_sort):
             if query_sort == "recently added on top":
                 query_set = query_set.order_by('-creation_date')
             if query_sort == "oldest on top":
-                query_set = query_set.order_by('creation_date')    
+                query_set = query_set.order_by('creation_date')
             if query_sort == "best score on top":
-                query_set = query_set.order_by('-score')    
-    paginator = Paginator(query_set,2)
+                query_set = query_set.order_by('-score')
+    paginator = Paginator(query_set, 2)
     page = request.GET.get('page')
     try:
         tickets = paginator.page(page)
@@ -42,5 +44,8 @@ def search(request):
         tickets = paginator.page(1)
     except EmptyPage:
         tickets = paginator.page(paginator.num_pages)
-        
-    return render(request,"ticketslist.html",{"tickets":tickets})
+
+    filter = {'keyword': query_keyword, 'status': query_status,
+              'label': query_label, 'sort': query_sort}
+
+    return render(request, "ticketslist.html", {"tickets": tickets, "filter": filter})
