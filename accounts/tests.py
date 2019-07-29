316c6d90ce32a8923c69793fa1cd5dcab437c86f
Irina Nita <irinanita.im@gmail.com>
2019-07-29 23:43:49 +0100

    Added tests for "user registration" and "add ticket" forms

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .forms import UserRegistrationForm
+
 
 # Create your tests here.
+class TestUserRegistrationForm(TestCase):
+    def test_impossible_to_register_with_only_name(self):
+        form = UserRegistrationForm({'username': 'Create Tests'})
+        self.assertFalse(form.is_valid())
+
+    def test_correct_message_for_missing_name(self):
+        form = UserRegistrationForm({'username': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+    def test_correct_message_when_passwords_dont_match(self):
+        form = UserRegistrationForm({'password1': '123abc', 'password2': '123xyz'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'], [u'Passwords must match'])
+
+    def test_form_valid_when_all_field_requirements_met(self):
+        form = UserRegistrationForm(
+            {'password1': '123abc', 'password2': '123abc', 'username': 'Test', 'email': 'test@tst.com'})
+        self.assertTrue(form.is_valid())
--- tickets/tests.py
+++ tickets/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .forms import AddTicketForm
+
 
 # Create your tests here.
+
+
+class TestAddTicketForm(TestCase):
+    # Imitates a manual DOM manipulation. Trying to pass a value that is not supported
+    def test_impossible_to_submit_ticket_with_wrong_type(self):
+        form = AddTicketForm(
+            {'title': 'Test', 'type': 'some-random-type', 'label': 'design', 'description': 'test', 'image': 'i.png'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['type'],
+                         [u'Select a valid choice. some-random-type is not one of the available choices.'])
+
+    def test_impossible_to_submit_ticket_with_wrong_label(self):
+        form = AddTicketForm(
+            {'title': 'ABC', 'type': 'bug', 'label': 'ux', 'description': 'test', 'image': 'i.png'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['label'], [u'Select a valid choice. ux is not one of the available choices.'])
+
+    # Image is optional so this test is supposed to pass
+    def test_submit_ticket_with_no_img(self):
+        form = AddTicketForm(
+            {'title': 'XYZ', 'type': 'bug', 'label': 'design', 'description': 'test', 'image': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_impossible_to_submit_ticket_with_no_title(self):
+        form = AddTicketForm(
+            {'title': '', 'type': 'bug', 'label': 'design', 'description': 'test', 'image': 'i.png'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['title'], [u'This field is required.'])
